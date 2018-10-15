ce672cbf2f4364b013859530480aba7da75acfb2
Deep Patel <dpatel39@students.towson.edu>
2018-10-15 17:11:41 -0400

    Initial Commit

---
+++ app/src/main/java/edu/towson/cosc431/Patel/CoolDictionary/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.towson.cosc431.Patel.CoolDictionary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
