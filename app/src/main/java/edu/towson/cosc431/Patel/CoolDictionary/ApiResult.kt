9d66081b849c57d4ab7d2e6d207132cb29ef30e0
Deep Patel <dpatel39@students.towson.edu>
2018-11-14 16:03:02 -0500

    Network call to api added

---
+++ app/src/main/java/edu/towson/cosc431/Patel/CoolDictionary/ApiResult.kt
@@ -0,0 +1,3 @@
+package edu.towson.cosc431.Patel.CoolDictionary
+
+data class ApiResult(val data: String)
--- app/src/main/java/edu/towson/cosc431/Patel/CoolDictionary/MainActivity.kt
+++ app/src/main/java/edu/towson/cosc431/Patel/CoolDictionary/MainActivity.kt
@@ -1,12 +1,59 @@
 package edu.towson.cosc431.Patel.CoolDictionary
 
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URL
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.error.ANError
+
+import com.androidnetworking.interfaces.ParsedRequestListener
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        searchBtn.setOnClickListener { searchWord() }
+
+        AndroidNetworking.initialize(this)
+        AndroidNetworking.enableLogging()
+
     }
+
+
+    fun searchWord() {
+
+        var word = editText.text
+        val API_URL = "https://googledictionaryapi.eu-gb.mybluemix.net/?define="
+        val url = URL("${API_URL}${word}&lang=en")
+
+        AndroidNetworking.get(url.toString())
+                .build()
+                .getAsObject(ApiResult :: class.java, object: ParsedRequestListener<ApiResult> {
+                   override fun onResponse(response: ApiResult?) {
+                       if(response == null){
+                           Log.d("manohman", "Null bro")
+                       }
+                       if(response != null) {
+                           Log.d("manohman", url.toString())
+                           Log.d("manohman", response.toString())
+                        }
+                    }
+
+                    override fun onError(anError: ANError?) {
+                        Toast.makeText(this@MainActivity, "Failed to download user list", Toast.LENGTH_SHORT).show()
+                    }
+                })
+
+
+
+    }
+
 }
+
